16c4c18ea80c8e3b8fa6634db6536b211cf33c25
Antony Holmes <antony@antonybholmes.com>
2024-04-20 12:50:49 -0400

    fix: Bug fixes and updates.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,90 @@
+use std::{fmt::Display, fs::File};
+
 mod tests;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone)]
+pub enum MicroarrayError {
+    FileError(String),
 }
 
+// impl From< std::error::Error> for MicroarrayError {
+//     fn from(error:  Error) -> Self {
+//         MicroarrayError::FileError(error.to_string())
+//     }
+// }
+
+//impl std::error::Error for AuthError {}
+
+impl Display for MicroarrayError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MicroarrayError::FileError(user) => {
+                write!(f, "account for {} does not exist", user)
+            }
+        }
+    }
+}
+
+pub fn make_tsv() -> Result<String, MicroarrayError> {
+    let file = match File::open("../../data/microarray/hgu133plus2.rma.collapsed.tsv") {
+        Ok(val) => val,
+        _ => return Err(MicroarrayError::FileError("file issue".to_string())),
+    };
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .delimiter(b'\t')
+        .from_reader(file);
+
+    let mut wtr = csv::WriterBuilder::new()
+        .delimiter(b'\t')
+        //.quote_style(csv::QuoteStyle::NonNumeric)
+        .has_headers(true)
+        .quote_style(csv::QuoteStyle::Never)
+        .from_writer(vec![]);
+
+    let cols = vec![0, 1, 4];
+
+    let headers = match rdr.headers() {
+        Ok(val) => val,
+        _ => return Err(MicroarrayError::FileError("header issue".to_string())),
+    };
+
+    //println!("{:?}", &cols.into_iter().map(|c|&headers[c]).collect::<Vec<&str>>());
+
+    let header = cols.iter().map(|c| &headers[*c]).collect::<Vec<&str>>();
+
+    match wtr.write_record(&header) {
+        Ok(val) => val,
+       _ => return Err(MicroarrayError::FileError("header issue".to_string())),
+    };
+
+    println!("{:?}", headers);
+
+    for result in rdr.records() {
+        let record = match result {
+            Ok(val) => val,
+            _ => return Err(MicroarrayError::FileError("header issue".to_string())),
+        };
+
+
+        let row = cols.iter().map(|c| &record[*c]).collect::<Vec<&str>>();
+        //println!("{}", &record[0]);
+        match wtr.write_record(&row) {
+            Ok(val) => val,
+            _ => return Err(MicroarrayError::FileError("header issue".to_string())),
+        };
+    }
+
+    let vec = match wtr.into_inner() {
+        Ok(val) => val,
+        _=> return Err(MicroarrayError::FileError("header issue".to_string())),
+    };
+
+    let data = match String::from_utf8(vec){
+        Ok(val) => val,
+        _ => return Err(MicroarrayError::FileError("header issue".to_string())),
+    };
  
+    Ok(data)
+}
